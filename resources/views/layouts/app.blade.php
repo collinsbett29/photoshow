96a4ce6e2defc04238fb4ef8ca6d1fdfaee47373
Collins Bett <collinsbett@Collinss-MacBook-Pro.local>
2021-10-29 14:23:21 +0300

    Add bootstrap links

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,9 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Photoshow</title>
-    <link rel="stylesheet" href="/css/app.css">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <script src="{{ asset('js/app.js') }}"></script>
 </head>
 <body>
+    @include('inc.navbar')
     @yield('content')
 </body>
 </html>
