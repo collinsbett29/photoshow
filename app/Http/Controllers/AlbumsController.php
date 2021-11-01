c96317fce123dfa8ab16978357aef7c053005da6
Collins Bett <collinsbett@Collinss-MacBook-Pro.local>
2021-11-01 17:27:38 +0300

    Update image cover upload

--- app/Http/Controllers/AlbumsController.php
+++ app/Http/Controllers/AlbumsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Album;
 
 class AlbumsController extends Controller
 {
@@ -13,6 +14,28 @@ class AlbumsController extends Controller
         return view('albums.create');
     }
     public function store(Request $request){
-        dd($request);
+        $this->validate($request,[
+            'name' => 'required',
+            'description' => 'required',
+            'cover-image' => 'required|image',
+        ]);
+
+        $filenameWithExtension = $request->file('cover-image')->getClientOriginalName();
+
+        $filename =pathinfo($filenameWithExtension, PATHINFO_FILENAME);
+        
+        $extension = $request->file('cover-image')->getClientOriginalExtension();
+
+        $filenameToStore = $filename . '_' . time() . '.' . $extension;
+
+        $path = $request->file('cover-image')->storeAs('public/album_covers', $filenameToStore);
+
+        $album = new Album ();
+        $album->name = $request->input('name');
+        $album->description = $request->input('description');
+        $album->cover_image = $filenameToStore;
+        $album->save();
+
+        return redirect('/albums')->with('success', 'Album created successfully!');
     }
 }
